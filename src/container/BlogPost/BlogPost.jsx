97c64ffb815c5322dd4efbd631c014cd8f3c2641
fransiskalidya <fransiskaly@gmail.com>
2022-03-10 16:24:15 +0700

    praktikum get API & praktikum 3

---
+++ src/container/BlogPost/BlogPost.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import './BlogPost.css';
+import Post from '../../component/BlogPost/Post';
+
+class BlogPost extends Component{
+    state={
+        listArtikel: []
+    }
+
+    ambilDataDariServerAPI= ()=>{
+        fetch('http://localhost:3001/posts')
+            .then(Response => Response.json())
+            .then(jsonHasilAmbilDariApi =>{
+                this.setState({
+                    listArtikel:jsonHasilAmbilDariApi
+                })
+            })
+    }
+
+    componentDidMount(){
+        this.ambilDataDariServerAPI()
+    }
+
+    handleHapusArtikel = (data)=>{
+        fetch('http://localhost:3001/posts/${data}', {method: 'DELETE'})
+            .then(res=>{
+                this.ambilDataDariServerAPI()
+            })
+    }
+
+    render(){
+        return (
+            <div className="post-artikel">
+                <h2>Daftar Artikel</h2>
+                {
+                    this.state.listArtikel.map(artikel=>{
+                        return <Post key={artikel.id} judul={artikel.title} isi={artikel.body} idArtikel={artikel.id} hapusArtikel={this.handleHapusArtikel}/>
+
+                    })
+                }
+
+
+            </div>
+            
+        )
+    }
+}
+
+export default BlogPost;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Hello = () =>{
-  return <p>hello</p>
+import HelloComponent from './component/HelloComponent';
+// import StateFullComponent from './container/StateFullComponent';
+import Test from './Test';
+import BlogPost from './container/BlogPost/BlogPost'
+
+// const Hello = () =>{
+//   return <p>hello</p>
+// }
+
+// const HelloComponent =()=>{
+//   return <p>HelloComponent</p>
+// }
+
+class StateFullComponent extends React.Component{
+  render(){
+    return <p>StateFullComponent</p>
+  }
 }
 
 
 ReactDOM.render(
-    <Hello />,
-  document.getElementById('root')
+    <BlogPost />,
+  document.getElementById('content')
 );
 
 // If you want to start measuring performance in your app, pass a function
